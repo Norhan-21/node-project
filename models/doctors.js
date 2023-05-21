495d477ad183de5bd655d6a3efb4626afbf23886
Nadaahmed123 <nadaahmed6512@gmail.com>
2023-05-21 17:23:52 +0200

    done

--- models/doctors.js
+++ models/doctors.js
@@ -9,7 +9,11 @@ const doctors = new Schema({
     type:String,
     required:true,
    },
-   
+   subject: {
+      type: Schema.Types.ObjectId,
+      required: false,
+      ref: 'subject'
+  },
    email:{
     type:String,
     required:true,
